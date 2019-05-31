cbf8d19b0d8430fc3e4bfaf9d25b9d926d6bc180
Stark <mrcs.stark@gmail.com>
2019-05-31 20:11:47 -0300

    Implementando models para Endereco e Pessoas

--- sinan/core/admin.py
+++ sinan/core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Endereco, Pessoa
 # Register your models here.
+
+
+admin.site.register(Endereco)
+admin.site.register(Pessoa)
--- sinan/core/models.py
+++ sinan/core/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Endereco(models.Model):
+    estado = models.CharField(max_length=80)
+    cidade = models.CharField(max_length=255)
+    bairro = models.CharField(max_length=500)
+    rua = models.CharField(max_length=500)
+    numero = models.IntegerField()
+    cep = models.CharField(max_length=9, blank=True, null=True)
+
+    def __str__(self):
+        return "%s, %s, %s, %s - %s " % (self.rua, self.numero, self.bairro, self.cidade, self.estado)
+
+
+class Pessoa(models.Model):
+    SEXO_CHOICE = (
+        ('HOMEM', 'Homem'),
+        ('MULHER', 'Mulher')
+    )
+    nome_completo = models.CharField(blank=True, max_length=500)
+    endereco = models.OneToOneField(Endereco, on_delete=models.CASCADE, null=True, blank=True,
+                                    related_name='usuario_endereco', unique=True)
+
+    telefone = models.CharField(max_length=50, blank=False, null=False)
+    sexo = models.CharField(max_length=50, choices=SEXO_CHOICE, blank=True, null=True)
+
+    def __str__(self):
+        return self.nome_completo
