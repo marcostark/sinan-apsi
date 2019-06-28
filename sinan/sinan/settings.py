9a084728b2c8c5ad586bfb4641f365ea22bb7e32
Marcos <mrcs.stark@gmail.com>
2019-06-28 18:41:16 -0300

    Deploy da aplicação

--- sinan/sinan/settings.py
+++ sinan/sinan/settings.py
@@ -65,7 +65,8 @@ ROOT_URLCONF = 'sinan.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS':[],
+        # 'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
